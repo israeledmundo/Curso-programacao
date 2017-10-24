f68f03ef82e9236a0cfaef6e8027117d7aadefd3
israeledmundo <33060508+israeledmundo@users.noreply.github.com>
2017-10-24 11:39:05 -0200

    Add files via upload

---
+++ Mundo_01/Desafio06.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número:'))
+#d = n * 2
+#t = n * 3
+#r = n **(1/2)
+print('O dobro do número {} é {}, o triplo é {} e a sua raiz quadrada é {:.2f}'.format(n,(n*2),(n*3),(n**(1/2))))
+#print('O dobro do número {} é {}, o triplo é {} e sua raiz quadrada é {}'.format(n,d,t,r))
