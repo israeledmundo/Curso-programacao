c66643228477528e65ce7d1834e326dc2732f0f9
israeledmundo <33060508+israeledmundo@users.noreply.github.com>
2017-10-26 13:14:43 -0200

    Update Desafio48.py

--- Mundo_02/Desafio48.py
+++ Mundo_02/Desafio48.py
@@ -1,9 +1,13 @@
 #coding: utf-8
 
-s = 0
-for c in range(1, 501):
+#acumulador
+soma = 0
+#contador
+cont = 0
+for c in range(1, 501, 2):
     if c % 3 == 0:
-        s += c
-print('A soma dos números múltiplos de 3 entre 1 e 500 é {}.'.format(s))
+        soma = soma + c
+        cont = cont + 1
+print('A soma dos números múltiplos de 3({} valores) entre 1 e 500 é {}.'.format(cont, soma))
 
 
