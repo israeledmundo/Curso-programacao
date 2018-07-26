dfd9ed095552c44d3575a07dce082778000f0d3a
israeledmundo <33060508+israeledmundo@users.noreply.github.com>
2018-07-26 17:32:08 -0300

    Update Desafio51.py

--- Mundo_02/Desafio51.py
+++ Mundo_02/Desafio51.py
@@ -1,6 +1,6 @@
 primeiro = int(input('Primeiro termo: '))
 razão = int(input('Razão: '))
-décimo = primeiro + (10-1)* razão
+décimo = primeiro + (10-1)* razão  #Decimo termo
 for c in range(primeiro, décimo + razão, razão):
     print('{} '.format(c),end= '-> ')
 print('ACABOU')
