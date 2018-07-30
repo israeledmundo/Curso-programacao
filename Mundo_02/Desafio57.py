b3e26c0d09facec078499032b76d82b49ea24331
israeledmundo <33060508+israeledmundo@users.noreply.github.com>
2018-07-30 17:04:44 -0300

    Create Desafio57.py

---
+++ Mundo_02/Desafio57.py
@@ -0,0 +1,6 @@
+#coding: UTF-8
+
+sexo = str(input('Informe seu sexo[M/F]: ')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo[M/F]:')).strip().upper()[0]
+print('Sexo {} registrado com sucesso.'.format(sexo))
