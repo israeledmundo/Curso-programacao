00d91d1f738e48e8c3ed9a60bc87799b357f2d42
israeledmundo <33060508+israeledmundo@users.noreply.github.com>
2018-08-01 17:03:29 -0300

    Create Desafio63.py

---
+++ Mundo_02/Desafio63.py
@@ -0,0 +1,19 @@
+#coding: UTF-8
+
+print('-'*30)
+print('Sequência de Fibonacci')
+print('-'*30)
+n = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+print('~'*30)
+print('{} -> {}'.format(t1,t2),end='')
+cont = 3
+while cont <= n:
+    t3 = t1+t2
+    print('-> {}'.format(t3),end='')
+    t1=t2
+    t2=t3
+    cont = cont + 1
+print('-> FIM')
+print('~'*30)
