6b22073d3c40d879c6fc5743291aea79e2509a2b
israeledmundo <33060508+israeledmundo@users.noreply.github.com>
2019-03-26 14:54:48 -0300

    Delete Desafio74.py

--- Desafio74.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-num = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
-print(f'Eu sorteei os valores: ', end='')
-for n in num:
-    print(f'{n} ',end='')
-print(f'\nO MAIOR valor sorteado foi {max(num)}.')
-print(f'O MENOR valor sorteado foi {min(num)}.')
