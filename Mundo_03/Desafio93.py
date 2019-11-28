8cf56f4c46ad8e63755d8c8af90b0c9c81e03e4f
israeledmundo <33060508+israeledmundo@users.noreply.github.com>
2019-11-28 14:47:21 -0300

    Update Desafio93.py

--- Mundo_03/Desafio93.py
+++ Mundo_03/Desafio93.py
@@ -1,16 +1,20 @@
-info = {}
+jogador = {}
 partidas = []
-info['Nome'] = str(input('Nome do jogador: '))
-tot = int(input('Quantas partidas: '))
-for g in range(0,tot):
-    partidas.append(int(input(f'Quantos gols na partida {g+1}: ')))
-info['Gols'] = partidas[:]
-info['Total'] = sum(partidas)
-print('=-'*30)
-for k, v in info.items():
-    print(f' O campo {k} tem valor {v}.')
-print('=-'*30)
-print(f'O jogador {info["Nome"]} jogou {len(info["Gols"])} partidas.')
-for i, v in enumerate(info['Gols']):
-    print(f'    => Na partida {i+1}, fez {v} gols')
-print(f'Foi um total de {info["Total"]} gols.')
+jogador['Nome'] = str(input('Nome do jogador: '))
+tot = int(input(f'Quantas partidas {jogador["Nome"]} jogou? '))
+for c in range(0,tot):
+    partidas.append(int(input(f'   Quantos gols na partida {c+1}? ')))
+jogador["Gols"] = partidas[:]
+jogador['Total'] = sum(partidas)
+print('-='*30)
+print(jogador)
+print(partidas)
+print('-='*30)
+for k,v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-=' * 30)
+
+print(f'O jogador {jogador["Nome"]} jogou {len(jogador["Gols"])} partidas.')
+for i, v in enumerate(jogador["Gols"]):
+    print(f'   ===> Na partida {i} fez {v} gols.')
+print(f'Foi um total de {jogador["Total"]} gols.')
