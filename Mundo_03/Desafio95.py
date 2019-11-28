fb2db301c3402f733f85344e0245c4cdc3785cfb
israeledmundo <33060508+israeledmundo@users.noreply.github.com>
2019-11-28 15:27:18 -0300

    Create Desafio95.py

---
+++ Mundo_03/Desafio95.py
@@ -0,0 +1,44 @@
+time = []
+jogador = {}
+partidas = []
+while True:
+    jogador.clear()
+    jogador['Nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas {jogador["Nome"]} jogou? '))
+    partidas.clear()
+    for c in range(0,tot):
+        partidas.append(int(input(f'   Quantos gols na partida {c+1}? ')))
+    jogador["Gols"] = partidas[:]
+    jogador['Total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar?[S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print("ERRO! Responda apenas com S ou N.")
+    if resp == "N":
+        break
+print('-=' * 30)
+print('cod ',end='')
+for i in jogador.keys():
+    print(f'{i:<15}',end='')
+print()
+print('-=' * 30)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}',end='')
+    print()
+print('-' * 30)
+while True:
+    busca = int(input('Mostrar dados de qual jogador? (999 para parar)'))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO! Não existe jogador com o código {busca}.')
+    else:
+        print(f'-- LEVANTAMENTO DO JOGADOR {time[busca]["Nome"]}: ')
+        for i, g in enumerate(time[busca]["Gols"]):
+            print(f'    No jogo {i+1} fez {g} gols.')
+    print('-')
+print('<<VOLTE SEMPRE>>')
